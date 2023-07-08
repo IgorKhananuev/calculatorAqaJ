ad222dc0fcb526325bef3411e021703eace27a5d
i.khananuev <i.khananuev@bftcom.com>
2023-07-08 20:40:29 +0300

    calculator v0.0.4

--- src/main/java/CalculatorApp.java
+++ src/main/java/CalculatorApp.java
@@ -2,12 +2,13 @@ import util.Util;
 import java.util.Scanner;
 
 import static calculator.Calculator.calc;
+import static util.Util.readInput;
 
 public class CalculatorApp {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            String inputValue = Util.readInput(scanner);
+            String inputValue = readInput(scanner);
             calc(inputValue);
         } catch (IllegalArgumentException e) {
             System.out.printf("Неверный результат: %s", e.getMessage());
