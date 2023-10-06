d1209384865a747f237814ac1ab63133a3f8cb97
i.khananuev <i.khananuev@bftcom.com>
2023-10-06 11:46:54 +0300

    add static import method calc in tests

--- src/java/test/java/calculator/CalculatorTest.java
+++ src/java/test/java/calculator/CalculatorTest.java
@@ -3,27 +3,28 @@ package calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.testng.annotations.Test;
+import static calculator.Calculator.calc;
 
 
 public class CalculatorTest {
 
     @Test(description = "тест на арифметические операции")
     public void calcTest() {
-        assertEquals("2", Calculator.calc("1 + 1"));
-        assertEquals("0", Calculator.calc("1 - 1"));
-        assertEquals("1", Calculator.calc("1 / 1"));
-        assertEquals("4", Calculator.calc("2 * 2"));
+        assertEquals("2", calc("1 + 1"));
+        assertEquals("0", calc("1 - 1"));
+        assertEquals("1", calc("1 / 1"));
+        assertEquals("4", calc("2 * 2"));
     }
 
     @Test(description = "тест на валидность вводимых значений")
     public void invalidOperationsTest() {
         Class<IllegalArgumentException> expectedException = IllegalArgumentException.class;
 
-        assertThrows(expectedException, () -> Calculator.calc("1 - 1 + 1"));
-        assertThrows(expectedException, () -> Calculator.calc("0 - 10"));
-        assertThrows(expectedException, () -> Calculator.calc("10 - 0"));
-        assertThrows(expectedException, () -> Calculator.calc("1 ? 1"));
-        assertThrows(expectedException, () -> Calculator.calc("1 + "));
-        assertThrows(expectedException, () -> Calculator.calc("1+1"));
+        assertThrows(expectedException, () -> calc("1 - 1 + 1"));
+        assertThrows(expectedException, () -> calc("0 - 10"));
+        assertThrows(expectedException, () -> calc("10 - 0"));
+        assertThrows(expectedException, () -> calc("1 ? 1"));
+        assertThrows(expectedException, () -> calc("1 + "));
+        assertThrows(expectedException, () -> calc("1+1"));
     }
 }
